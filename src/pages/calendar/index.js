f7870b233679b87363ed1e649c2de9439555b8ec
alexisraimbault <42438383+alexisraimbault@users.noreply.github.com>
2020-06-06 01:48:13 +0200

    fixed week start wrong by one day and displaying year in navigation

--- src/pages/calendar/index.js
+++ src/pages/calendar/index.js
@@ -157,7 +157,7 @@ class Calendar extends Component {
 
 
   getDayFromWeekAndYear = (week, year) => {
-    return moment().day("Monday").year(year).week(week).toDate();
+    return moment().day("Monday").year(year).week(week).add(1, 'd').toDate();
   };
 
   getRecipients = (events) => {
@@ -198,7 +198,7 @@ class Calendar extends Component {
     let daysOfWeek = [];
     
     _.forEach(_.times(5), item => {
-      const day = moment(weekStart).add(item + 1, 'd')._d;
+      const day = moment(weekStart).add(item, 'd')._d;
       daysOfWeek.push(
         {
           day: day,
@@ -224,22 +224,27 @@ class Calendar extends Component {
         />
         <div className="week-navigation-container">
           <div className="week-navigation-holder">
-            <div className="calendar-navigation-container" onClick={this.navigateToPreviousWeek}>
-              <svg viewBox="0 0 512 512" width="20" height="20">
-                <path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12
-                  C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084
-                  c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864
-                  l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z"/>
-              </svg>
+            <div className="top">
+              <div className="calendar-navigation-container" onClick={this.navigateToPreviousWeek}>
+                <svg viewBox="0 0 512 512" width="20" height="20">
+                  <path d="M198.608,246.104L382.664,62.04c5.068-5.056,7.856-11.816,7.856-19.024c0-7.212-2.788-13.968-7.856-19.032l-16.128-16.12
+                    C361.476,2.792,354.712,0,347.504,0s-13.964,2.792-19.028,7.864L109.328,227.008c-5.084,5.08-7.868,11.868-7.848,19.084
+                    c-0.02,7.248,2.76,14.028,7.848,19.112l218.944,218.932c5.064,5.072,11.82,7.864,19.032,7.864c7.208,0,13.964-2.792,19.032-7.864
+                    l16.124-16.12c10.492-10.492,10.492-27.572,0-38.06L198.608,246.104z"/>
+                </svg>
+              </div>
+              <div>{`${moment(weekStart).format("D MMM")} - ${moment(weekStart).add(4, 'd').format("D MMM")}`}</div>
+              <div className="calendar-navigation-container" onClick={this.navigateToNextWeek}>
+                <svg viewBox="0 0 512 512" width="20" height="20">
+                  <path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12
+                    c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028
+                    c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265
+                    c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"/>
+                </svg>
+              </div>
             </div>
-            <div>{`${moment(weekStart).format("D MMM")} - ${moment(weekStart).add(4, 'd').format("D MMM")}`}</div>
-            <div className="calendar-navigation-container" onClick={this.navigateToNextWeek}>
-              <svg viewBox="0 0 512 512" width="20" height="20">
-                <path d="M382.678,226.804L163.73,7.86C158.666,2.792,151.906,0,144.698,0s-13.968,2.792-19.032,7.86l-16.124,16.12
-                  c-10.492,10.504-10.492,27.576,0,38.064L293.398,245.9l-184.06,184.06c-5.064,5.068-7.86,11.824-7.86,19.028
-                  c0,7.212,2.796,13.968,7.86,19.04l16.124,16.116c5.068,5.068,11.824,7.86,19.032,7.86s13.968-2.792,19.032-7.86L382.678,265
-                  c5.076-5.084,7.864-11.872,7.848-19.088C390.542,238.668,387.754,231.884,382.678,226.804z"/>
-              </svg>
+            <div className="bottom">
+              {`${moment(weekStart).format("YYYY")}`}
             </div>
           </div>
         </div>
